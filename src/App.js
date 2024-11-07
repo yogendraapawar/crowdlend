516db59ef82de4a4f9b1052bbd1649f1b92dc9a4
Yogendra Pawar <yogendra.pawar@think360.ai>
2024-11-08 02:08:24 +0530

    ui changes

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
           <Route path="/dashboard" element={<DashboardLayoutBasic />} />
 
           <Route path="/login" element={<SignIn/>} />
+          <Route path="/agent" element={<SignIn/>} />
         </Routes>
       </div>
     </Router>
